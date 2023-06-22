6cc5e986c4a562c6181ee4ce5a685e1cffeab923
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-22 10:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+ function getMessage() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('Hello, world!');
+        }, 1000);
+      });
+    }
+
+    // Function to update the text of the output element with the resolved message
+    function updateOutput() {
+      const outputElement = document.getElementById('output');
+      getMessage()
+        .then((message) => {
+          outputElement.textContent = message;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    // Call the updateOutput() function to start the process
+    updateOutput();
